756f0d504235593baf9e9af8f52879c5353be0e1
mateomcallister <mateomcallisterschool@gmail.com>
2025-11-18 10:34:27 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
